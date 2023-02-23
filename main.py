8e2427e87aa8d6cd069a9683e94ea90e72bc3f3f
ambre66160 <louping.545samsam@gmail.com>
2023-02-23 10:01:13 +0100

    site

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #réaliser l'incorporation de bibliothèques
+from flask import Flask, flash, redirect, render_template, request, session, abort
+
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Coucou tout le monde"
+
+@app.route("/scan")
+def scan():
+    templateData= {
+        "title":"ekoroji",
+        "name" : "Ambre"
+    }
+    return render_template('index.html',**templateData)
+
+@app.route("/informer")
+def informer():
+    return " Voici les nouveaux articles"
+
+if __name__ == "__main__":
+    app.run(host = "127.0.0.1")
