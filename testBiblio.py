6e31eb909d24a977330998c45a0fd0a19522e74e
untypequicode <untypequicode@gmail.com>
2023-03-02 09:06:35 +0100

    modif css

--- testBiblio.py
+++
@@ -1,5 +0,0 @@
-from trad import *
-
-siteweb = TradHtml(["index.html", "styleindex.css", "messcans.html", "explorer.html", "stylemesscans.css", "styleindex.css"], "Test_projet1", "testtemplates/")
-siteweb.read()
-siteweb.tradFiles()
